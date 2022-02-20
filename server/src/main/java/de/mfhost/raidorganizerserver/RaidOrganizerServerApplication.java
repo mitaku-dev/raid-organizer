5c342865b2b6b6eee16095158a440c036c406da0
Michael <panp405@gmail.com>
2022-02-20 19:33:34 +0100

    init with SpringBoot Server

---
+++ server/src/main/java/de/mfhost/raidorganizerserver/RaidOrganizerServerApplication.java
@@ -0,0 +1,13 @@
+package de.mfhost.raidorganizerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaidOrganizerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaidOrganizerServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/de/mfhost/raidorganizerserver/RaidOrganizerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mfhost.raidorganizerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaidOrganizerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
